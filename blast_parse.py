5702b85ca95ee83ed9761a397ac8c6b17b0dc5d1
Justin Lorenzo Chavez <Justin_C@Justins-MacBook-Pro.local>
2015-02-20 16:04:31 -0500

    Found query length
    Ok I got the query length and the function now sorts by query coverage.
    What I did basically was use the gene id and used entrez (we used it
    before to get protein id) to get the protein sequence which I took the
    length of to get the protein sequence length of the original.

--- blast_parse.py
+++ blast_parse.py
@@ -2,8 +2,31 @@ from __future__ import division
 import math
 from decimal import *
 
+#Starting here is what I imported to get the protein sequence
+from Bio import Entrez
+from Bio import SeqIO
+Entrez.email = "nickyesupriya@hotmail.com"
+
+#this list holds the gene id for what we are quering with
+id_list = ['77462544']
+handle = Entrez.efetch(db="protein", id=id_list, rettype="fasta", retmode="text")   
+#the following are just extra details that I took out since they are not relevant
+
+#for seq_record in SeqIO.parse(handle, "fasta"):
+ #   print ">" + seq_record.id, seq_record.description
+#print seq_record.seq
+handle.close()
+
 
 result_handle = open("my_blast.out")
+
+#seq_record.seq holds the protein sequence, q_length will take the length of 
+#that to find query length, I placed it between the two print statements so
+#you can verify that the length is the actual length
+q_length = len(seq_record.seq)
+print "####################"
+print str(q_length)
+print "####################"
 
 from Bio.Blast import NCBIXML
 #Parses for multiple query sequences
@@ -17,7 +40,7 @@ count = 0
 for alignment in blast_record.alignments:
 	for hsp in alignment.hsps:
             
-            if (hsp.expect < E_VALUE_THRESH) and (Decimal(hsp.identities) / Decimal(alignment.length) > .9): 
+            if (hsp.expect < E_VALUE_THRESH) and (((hsp.query_end - hsp.query_start)/q_length) > .9): 
                   
                   count += 1
                   
@@ -31,10 +54,11 @@ for alignment in blast_record.alignments:
 #                  print "gaps" + str(hsp.gaps)
                   print "query" + str(hsp.query)
                   print "query_start" + str(hsp.query_start)
-                  print "query_start" + str(hsp.query_end)
+                  print "query_end" + str(hsp.query_end)
 #                  print "sbkct_start" + str(hsp.sbjct_start)
 #                  print "frame" + str(hsp.frame)
 #                  print "score:" + str(hsp.score)+ "\n"
+                  print "query length: " + str(len(hsp.query))
                   print ""
                   print ""
                   print ""
