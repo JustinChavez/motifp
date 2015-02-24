0d7510192f353c2d6c1ece901e017e34b2d47c10
Justin Lorenzo Chavez <Justin_C@Justins-MacBook-Pro.local>
2015-02-24 17:23:07 -0500

    gene id list added and error on fasta parser fixed

--- blast_seq.py
+++ blast_seq.py
@@ -7,40 +7,58 @@ from Bio.Blast import NCBIXML
 fasta_file = open("LexA_fasta.txt")
 i=0
 fasta_list = []
+gi_list = []
 flag = 0
 count = 0
 temp = ""
 read = False
+
 #iterate through each line of the file
 for line in fasta_file:
+    #print "1"
     
-   #indicates the start of a new paragraph in the file
+    
+#indicates the start of a new paragraph in the file
     if line[0:3] == ">gi":
         
-        #ensures we don't append when there is nothing there
-        if read == True:        
+#ensures we don't append when there is nothing there
+        if read == True:
             flag == 0
             fasta_list.append(temp)
             count += 1
         temp = line
         read = True
         flag = 1
-    #this indicates we are still reading the paragraph into the variable    
+        
+        #gets the length of the gene id
+        k = 4
+        while line[k] != "|":
+            k += 1
+            
+        gi_list.append(line[4:k])
+            #this indicates we are still reading the paragraph into the variable
     if flag == 1:
-        temp += line
+        
+        #makes sure we do not add the frist line twice
+        if line[0] != ">":
+            temp += line
+            #print temp
     
-    
+        
 print "there are: " + str(count) + "fasta in the list"
 print fasta_list
-            
-
-#f_record = next(SeqIO.parse('prot_fasta.txt', 'fasta'))
+print gi_list
+     
+#########################################
+#       
+#for gi in fasta_list:
+#    f_record = next(SeqIO.parse('fasta1.fasta', 'fasta'))
+#
 #
 #print ('Performing Blast')
 #result_handle = NCBIWWW.qblast("blastp", "nr", f_record.format('fasta'))
 #
-#save_file = open("my_blast.out", "w")
+#save_file = open("blast.out", "w")
 #save_file.write(result_handle.read())
 #save_file.close()
 #result_handle.close()
-
